b2ff2f7e39ab54b82f1cf5713ffc1ebfb2b4b1d0
HomoEfficio <homo.efficio@gmail.com>
2016-12-22 00:14:46 +0900

    Swagger2 Docket Bean 설정
    localhost:8080/v2/api-docs 로 설정 성공 확인 가능

---
+++ member/src/main/java/homo/efficio/micro/todo/member/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package homo.efficio.micro.todo.member.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author homo.efficio@gmail.com
+ *         create on 2016-12-22.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
